3dbfb0b44b420a73bd1a38d95bf68d7d5444b981
Oscar Lopez <osacarloopez85@gmail.com>
2025-04-24 16:15:22 -0400

    feat: Create function that allows the user to play a round against the computer

--- game.js
+++ game.js
@@ -1,11 +1,11 @@
 function getComputerChoice() {
-    let choice = Math.floor((Math.random() * 3));
+    const choice = Math.floor((Math.random() * 3));
     
     return getChoice(choice);
 }
 
 function getHumanChoice() {
-    let choice = 
+    const choice = 
         prompt("Enter an option from 1 to 3, (1 = rock, 2 = paper, 3 = scissors)");
     
     return getChoice(choice - 1);
@@ -25,4 +25,45 @@ function getChoice(choice) {
     }
 }
 
-console.log(getHumanChoice());
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+
+    const winnerValue = determineWinner(humanChoice, computerChoice);
+
+    switch(winnerValue) {
+        case 0:
+            console.log("it's a tie!");
+            break;
+        case 1:
+            console.log(`You Win! ${humanChoice} beats ${computerChoice}`);
+            break;
+        case 2:
+            console.log(`You Lose! ${computerChoice} beats ${humanChoice}`);
+
+    }
+}
+
+
+function determineWinner(player1, player2) {
+    if(player1 == player2) {
+        return 0;
+    }
+    if((player1 == "rock" && player2 == "scissors") ||
+        (player1 == "paper" && player2 == "rock") ||
+        (player1 == "scissors" && player2 == "paper")) {
+        humanScore++;
+        return 1;
+    }
+
+    else {
+        return 2;
+    }
+}
+
+
+const humanSelection = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+playRound(humanSelection, computerChoice);
