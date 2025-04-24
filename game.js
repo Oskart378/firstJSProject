53ddf1484e1ba3846eca71ef1775112a9dce0f9f
Oscar Lopez <osacarloopez85@gmail.com>
2025-04-24 15:30:47 -0400

    feat: Add fuction getComputerChoice to get a random choice from the computer

---
+++ game.js
@@ -0,0 +1,19 @@
+function getComputerChoice() {
+    let choice = Math.floor((Math.random() * 3));
+
+    
+    switch(choice) {
+        case 0: 
+            return "rock";
+        case 1:
+            return "paper";
+        case 2: 
+            return "scissors";
+        default:
+            return null;
+    }
+
+    
+}
+
+console.log(getComputerChoice());
