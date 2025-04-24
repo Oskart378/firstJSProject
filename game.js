14a0b98bbbcc587491f1a025edd8b3993c07d4ff
Oscar Lopez <osacarloopez85@gmail.com>
2025-04-24 15:37:55 -0400

    feat: Create function getHumanChoice to get human choice

--- game.js
+++ game.js
@@ -1,7 +1,18 @@
 function getComputerChoice() {
     let choice = Math.floor((Math.random() * 3));
-
     
+    return getChoice(choice);
+}
+
+function getHumanChoice() {
+    let choice = 
+        prompt("Enter an option from 1 to 3, (1 = rock, 2 = paper, 3 = scissors)");
+    
+    return getChoice(choice - 1);
+}
+
+function getChoice(choice) {
+
     switch(choice) {
         case 0: 
             return "rock";
@@ -12,8 +23,6 @@ function getComputerChoice() {
         default:
             return null;
     }
-
-    
 }
 
-console.log(getComputerChoice());
+console.log(getHumanChoice());
